71fbf0aa03acb7cbaf8c2638f1f70d5b3262f575
BHUVAN GS <bhuvi@BHUVANs-MacBook-Air.local>
2025-04-03 19:14:21 +0530

    Adding collect.sh file fot metrics parameter

---
+++ package/collect.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ensure gum and sysstat (mpstat) are installed
+if ! command -v gum &> /dev/null; then
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+    sudo apt update && sudo apt install gum
+fi
+
